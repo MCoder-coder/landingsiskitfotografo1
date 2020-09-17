26a5d120c5115924042ae241fc8900f985b3a2ba
KalCoder <jesusreeb@hotmail.com>
2020-09-17 00:07:41 -0300

    seccond commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import swiper, { Swiper } from 'swiper/bundle';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit ,AfterContentInit {
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(){
+
+
   }
 
 }
--- src/app/swiper/swiper.component.ts
+++ src/app/swiper/swiper.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import swiper, { Swiper } from 'swiper/bundle';
 
 @Component({
   selector: 'app-swiper',
   templateUrl: './swiper.component.html',
   styleUrls: ['./swiper.component.css']
 })
-export class SwiperComponent implements OnInit {
+export class SwiperComponent implements OnInit , AfterViewInit {
+
+
+  mySwiper: Swiper;
+
+
+  images: string[] = [
+    'assets/images/1.jpg',
+    'assets/images/2.jpg',
+    'assets/images/3.jpg',
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+       // If we need pagination
+       speed: 600,
+      parallax: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+    });
   }
 
 }
