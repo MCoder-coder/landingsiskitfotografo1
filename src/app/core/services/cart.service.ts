c29e8e964069a8cac77f68036dea304871836d03
kalcoder <jesusreeb@hotmail.com>
2021-01-20 18:01:54 -0300

    encriptacion del localStorage: dependencias npm install crypto-js@3.1.9-1 npm install crypto-js

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 import { Fotos } from '../models/fotos.model';
+import { LocalService } from "./local.service";
+import { Local } from 'protractor/built/driverProviders';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +17,8 @@ export class CartService {
   private tempCartItem: CartItem;
   //Behaviour Subject nos permite utilizar una característica realmente útil y que es la de poder "recodar¨ el último valor emitido por el Observable a todas las nuevas subscripciones, al margen del momento temporal en que éstas se establezcan
   private totalItems: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-
-  constructor() {
+  //inyecto servicio de encriptacion para acceder a sus metodos
+  constructor(private localService : LocalService) {
     // cargo el carrito desde el local storage
     this.loadLocalStorageCart();
     this.totalItems.next(this.getFotosCount());
@@ -23,18 +26,24 @@ export class CartService {
 
   loadLocalStorageCart() {
     //El método JSON.parse() analiza una cadena de texto como JSON, transformando opcionalmente  el valor producido por el análisis.
-    let myCart = JSON.parse(localStorage.getItem('cart'));
-    if (!myCart) {
+    //let myCart = JSON.parse(localStorage.getItem('cart'));
+     //local service inyectado en el constructo key cart para obtener los datos este metodo desencripta los datos guardados
+    //en localStorage
+     let get =this.localService.getJsonValue('cart')
+    if (!get) {
+      //si el carrito esta vacio
       this.cart = [];
     } else {
-      this.cart = myCart;
+      //sino el carrito esta llenos
+      this.cart = get;
     }
   }
 
   updateLocalStorageCart() {
     //El JSON.stringify()método convierte un objeto o valor de JavaScript en una cadena
-    let myCart = JSON.stringify(this.cart);
-    localStorage.setItem('cart', myCart);
+    this.localService.setJsonValue('cart' , this.cart)
+   // let myCart = JSON.stringify(this.cart);
+    //localStorage.setItem('cart', myCart);
   }
 
   addToCart(foto) {
@@ -52,7 +61,7 @@ export class CartService {
     console.log('cart', this.cart);
 
     this.updateLocalStorageCart();
-    //método next para emitir valores
+    //método next para emitir valores en este caso las items sumados del cart
     this.totalItems.next(this.getFotosCount());
   }
 
@@ -83,7 +92,7 @@ export class CartService {
     let total: number = 0;
     for (let item of this.cart) {
       console.log(item.cantidad);
-
+      //suma el total que es igual a 0 por la la cantidad de item dentro del carrito
       total = Number(total) + Number(item.cantidad);
     }
     return total;
